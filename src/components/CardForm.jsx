3ecbc81c79255b40daac0682b528449cfce65d4c
Daniel692 <birkusdaniel6@gmail.com>
2022-10-18 15:57:54 +0200

    geolocation added

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,22 +1,44 @@
-import { IonItem, IonLabel, IonInput, IonTextarea, IonImg, IonButton, IonIcon } from "@ionic/react";
+import { IonItem, IonLabel, IonInput, useIonAlert, IonTextarea, IonImg, IonButton, IonIcon } from "@ionic/react";
 import { useState, useEffect } from "react";
 import { Camera, CameraResultType } from "@capacitor/camera";
-import { camera } from "ionicons/icons";
+import { Geolocation } from "@capacitor/geolocation";
+import { camera, locationOutline } from "ionicons/icons";
 
 function FormCard( {post, handleSubmit} ) {
     const [title, setTitle] = useState("")
     const [userName, setUserName] = useState("")
     const [body, setBody] = useState("")
+    const [location, setLocation] = useState("");
     const [image, setImage] = useState("")
     const imgPlaceholder = "https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
     
+    const [presentLocationDialog] = useIonAlert();
     const [error, setError] = useState("") 
+
+    async function getCurrentPosition() {
+        const coordinates = await Geolocation.getCurrentPosition();
+        console.log("Current position:", coordinates);
+        getLocation(coordinates.coords.latitude, coordinates.coords.longitude);
+    }
+
+    async function getLocation(latitude, longitude) {
+        const key = "14de60fd94d4d8753fe8a277ba88667a";
+        const res = await fetch(
+            `http://api.positionstack.com/v1/reverse?access_key=${key}&output=json&query=${latitude},${longitude}`
+        );
+        const result = await res.json();
+        console.log(result);
+        const loc = result.data[0];
+        console.log(loc);
+        setLocation(loc.locality);
+    }
 
     useEffect(() => {
         if(post) {
             setTitle(post.title)
             setUserName(post.uid)
             setBody(post.body)
+            setLocation(post.location)
             setImage(post.image)
         }
     }, [post])
@@ -36,7 +58,7 @@ function FormCard( {post, handleSubmit} ) {
 
     const submitForm = (event) => {
         event.preventDefault()
-        const formData = {title: title, uid: userName, body: body, image: image}
+        const formData = {title: title, uid: userName, body: body, location: location, image: image}
         // handleSubmit(formData)
         if (formData.title === "" || formData.uid === "" || formData.body === "" || formData.image === "") {
             setError(
@@ -46,9 +68,24 @@ function FormCard( {post, handleSubmit} ) {
             )
         } else {
             handleSubmit(formData)
-        }
-        
+        }        
     }
+    
+    function confirmLocation() {
+        presentLocationDialog({
+            title: 'Confirm location',
+            message: 'Do you want to use Precise Location?',
+            buttons: [
+                {
+                    text: 'No',
+                },
+                {
+                    text: 'Yes',
+                    handler: getCurrentPosition
+                }
+            ]
+        })
+      }
 
     return (
         <form onSubmit={submitForm}>
@@ -65,15 +102,20 @@ function FormCard( {post, handleSubmit} ) {
                 <IonTextarea value={body} placeholder="Type your description here" onIonChange={event => setBody(event.target.value)}></IonTextarea>
             </IonItem>
             <IonItem>
-                <IonLabel position="stacked">Image URL</IonLabel>
-                <IonInput value={image} placeholder="Choose the URL for your image" onIonChange={event => setImage(event.target.value)}></IonInput>
+                <IonLabel>Location:</IonLabel>
+                <IonInput value={location} placeholder="Choose your location" onIonChange={event => setLocation(event.target.value)}></IonInput>
+                <IonButton onClick={confirmLocation}>
+                <ion-icon slot="icon-only" icon={locationOutline}></ion-icon>
+                </IonButton>
             </IonItem>
-            <IonItem onClick={takePicture} lines="none">
-                <IonLabel>Choose Image</IonLabel>
-                <IonButton>
+            <IonItem>
+                <IonLabel>Image:</IonLabel>
+                <IonInput value={image} placeholder="Choose image URL" onIonChange={event => setImage(event.target.value)}></IonInput>
+                <IonButton onClick={takePicture}>
                     <IonIcon slot="icon-only" icon={camera} />
                 </IonButton>
             </IonItem>
+
                 {error}
                 <IonImg style={{height: "200px"}} className="ion-padding" src={image === "" ? imgPlaceholder : image} ></IonImg>
                 <IonButton type="submit" expand="block">Save</IonButton>
